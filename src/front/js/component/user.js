767561cdd6f3ff8690d5480862d54cdab64a705c
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-12-15 19:15:59 +0000

    Lápiz de editar perfil habilita edición en inputs

--- src/front/js/component/user.js
+++ src/front/js/component/user.js
@@ -6,6 +6,11 @@ const User = () => {
         setShowPasswordFields(!showPasswordFields);
     };
 
+    const [isEditable, setIsEditable] = useState(false);
+    const toggleEditMode = () => {
+        setIsEditable(!isEditable);
+    }
+
     return (
         <div className="container mt-4">
             <div className="row md-5 p-4 bg-success rounded-1 align-items-start">
@@ -14,9 +19,23 @@ const User = () => {
                 <div className="col-12 col-md-6 mb-4">
                     <div className="row">
                         {/* Título "Mi perfil" */}
-                        <div className="col-12 d-flex align-items-center">
+                        <div className="col-12 d-flex justify-content-between align-items-center pb-2">
                             <h4 className="user-title adlam-display-regular">Mi perfil</h4>
-                            <h6 className="ms-2 fa-solid fa-pencil mt-2"></h6>
+
+                            {/* Contenedor para el lápiz y el texto */}
+                            <div className="position-relative">
+                                <h6
+                                    className="fa-solid fa-pencil mt-2 user-title"
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={toggleEditMode}
+                                ></h6>
+
+                                {/* Texto "Editar perfil" solo visible cuando el cursor pasa por encima */}
+                                <span className="hover-text position-absolute end-100 top-50 translate-middle-y ms-2 text-primary">
+                                    Editar perfil
+                                </span>
+                            </div>
+
                         </div>
                     </div>
                     {/* Tabla de Mi Perfil */}
@@ -24,11 +43,11 @@ const User = () => {
                         <form>
                             <div className="mb-3">
                                 <label htmlFor="nombre" className="form-label">Nombre</label>
-                                <input type="text" id="nombre" className="form-control" placeholder="Nombre de usuario" disabled />
+                                <input type="text" id="nombre" className="form-control" placeholder="Nombre de usuario" disabled={!isEditable} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" id="email" className="form-control" placeholder="Ingresa tu email" readOnly />
+                                <input type="email" id="email" className="form-control" placeholder="Ingresa tu email" disabled={!isEditable} />
                             </div>
                             <div className="mb-3">
                                 <a className="text-primary nunito" onClick={togglePasswordFields} style={{ cursor: 'pointer' }}>Cambiar contraseña</a>
@@ -46,6 +65,21 @@ const User = () => {
                                     </div>
                                 </>
                             )}
+                            <div className="mb-3 user-title adlam-display-regular">
+                                <h5>Información de contacto</h5>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="phone" className="form-label">Teléfono</label>
+                                <input type="text" id="phone" className="form-control" placeholder="Ingresa tu teléfono" disabled={!isEditable} />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="instagram" className="form-label">Instagram</label>
+                                <input type="text" id="instagram" className="form-control" placeholder="Ingresa tu Instagram" disabled={!isEditable} />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="facebook" className="form-label">Facebook</label>
+                                <input type="text" id="facebook" className="form-control" placeholder="Ingresa tu Facebook" disabled={!isEditable} />
+                            </div>
                             <div className="d-grid gap-2 col-6 mx-auto">
                                 <button type="submit" className="btn btn-primary rounded-pill btnStart">Guardar</button>
                             </div>
@@ -55,7 +89,7 @@ const User = () => {
 
                 {/* Título y Tabla de Mis Publicaciones */}
                 <div className="col-12 col-md-6 mb-4">
-                    <div className="row">
+                    <div className="row pb-2">
                         {/* Título "Mis publicaciones" */}
                         <div className="col-12">
                             <h4 className="user-title adlam-display-regular">Mis publicaciones</h4>
