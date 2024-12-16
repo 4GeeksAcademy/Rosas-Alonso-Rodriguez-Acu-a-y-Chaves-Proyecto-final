a77c09a9d6fb75da9ca69f6828905f8d3adc1744
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-12-16 19:34:00 +0000

    Arreglé unos errores en consola que daba newPetLost

--- src/front/js/pages/newPetLost.js
+++ src/front/js/pages/newPetLost.js
@@ -70,7 +70,7 @@ const NewPetLost = () => {
             <label htmlFor="species" className="form-label">ESPECIE</label>
 
             <select className="form-select" id="species" aria-label="Default select example" value={newPet.species} required onChange={handleChange} >
-              <option value="" disabled selected>Selecciona</option>
+              <option value="" disabled>Selecciona</option>
               <option value="perro">Perro</option>
               <option value="gato">Gato</option>
               <option value="conejo">Conejo</option>
@@ -86,10 +86,10 @@ const NewPetLost = () => {
           </div>
           <div className="row g-3 mb-3 ">
             <div className="col-md-6">
-              <label htmlFor="gender" className="form-label">GENERO</label>
+              <label htmlFor="gender" className="form-label">GÉNERO</label>
 
               <select className="form-select" id="gender" aria-label="Selecionar genero" value={newPet.gender} placeholder="Selecciona" required onChange={handleChange} >
-                <option value="" disabled selected>Seleciona</option>
+                <option value="" disabled>Selecciona</option>
                 <option value="male">Macho</option>
                 <option value="female">Hembra</option>
                 
