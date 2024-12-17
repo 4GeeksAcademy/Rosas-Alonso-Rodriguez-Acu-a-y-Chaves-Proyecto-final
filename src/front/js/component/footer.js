5dee31ab8974a6b79af12bcfaa969882034801e4
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-12-17 16:19:20 +0000

    Agregué estilos al footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,9 +4,9 @@ import "../../styles/navbar.css";
 
 export const Footer = () => (
 	<footer className="footer mt-auto  text-center bg-white shadow-sm p-0 m-0">
-		<h6 className="p-0 adlam-display-minusc text-muted ">
+		<h6 className="p-0 adlam-display-regular text-muted ">
 			<img src={Logo} alt="logo" style={{ height: "60px", width: "auto", objectFit: "contain" }}/>
-			 Miles de historias felices nos respaldan
+			 Miles de historias felices nos respaldan ♦
 		</h6>
 	</footer>
 );
